e9812c65e95f277cae48d195a9e5d5d22f7d4df1
Raí Adriano <raivieiraadriano92@gmail.com>
2024-08-08 17:32:27 -0300

    feat: setup custom theme for react navigation

--- app/src/navigation/RootNavigator.tsx
+++ app/src/navigation/RootNavigator.tsx
@@ -1,6 +1,10 @@
 import { type FunctionComponent } from "react";
 
-import { NavigationContainer } from "@react-navigation/native";
+import {
+  DefaultTheme,
+  NavigationContainer,
+  Theme
+} from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import { ArticleScreen } from "src/screens/ArticleScreen";
 
@@ -10,8 +14,16 @@ import type { RootStackParamList } from "./types";
 
 const NativeStack = createNativeStackNavigator<RootStackParamList>();
 
+const theme: Theme = {
+  ...DefaultTheme,
+  colors: {
+    ...DefaultTheme.colors,
+    primary: "#FF6600"
+  }
+};
+
 export const RootNavigator: FunctionComponent = () => (
-  <NavigationContainer>
+  <NavigationContainer theme={theme}>
     <NativeStack.Navigator>
       <NativeStack.Screen
         component={TabNavigator}
