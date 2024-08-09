d65ef95fef85b2671bc033c4535026815125e0af
Raí Adriano <raivieiraadriano92@gmail.com>
2024-08-09 11:35:36 -0300

    feat: add WebView in ArticleScreen

--- app/src/screens/ArticleScreen.tsx
+++ app/src/screens/ArticleScreen.tsx
@@ -1,4 +1,6 @@
-import { View } from "react-native";
+import WebView from "react-native-webview";
 import { RootStackScreen } from "src/navigation/types";
 
-export const ArticleScreen: RootStackScreen<"Article"> = () => <View />;
+export const ArticleScreen: RootStackScreen<"Article"> = ({
+  route: { params }
+}) => <WebView className="flex-1" source={{ uri: params.story_url }} />;
